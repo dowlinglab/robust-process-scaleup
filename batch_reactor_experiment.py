2bc4fbc350a7ff9f47c9313f0294d1fb485dd5b8
slilonfe5 <slilonfe@nd.edu>
2026-05-31 13:37:19 -0400

    Updated files

--- batch_reactor_experiment.py
+++ batch_reactor_experiment.py
@@ -1,4 +1,5 @@
 import pyomo.environ as pyo
+import idaes
 import math
 from matplotlib.ticker import MaxNLocator
 from pyomo.network import Arc
@@ -38,7 +39,7 @@ class BatchReactorExperiment(Experiment):
     m: annotated Pyomo model of the batch reactor
     """
 
-    def __init__(self, XA0, temp_control, const_temp, data=None, doe_solve=False,):
+    def __init__(self, temp_control, const_temp, XA0=None, data=None, doe_solve=False,):
 
         self.data = data
         self.XA0 = XA0
@@ -55,11 +56,11 @@ class BatchReactorExperiment(Experiment):
 
     def create_model(self):
         if self.const_temp and not self.doe_solve:
-            self.model = reform_batch_reactor_model_const(self.XA0, self.temp_control)
+            self.model = reform_const_temp_reactor_model(self.XA0, self.temp_control)
         elif not self.const_temp and not self.doe_solve:
-            self.model = reform_batch_reactor_model_var(self.XA0, self.temp_control)
+            self.model = reform_var_temp_reactor_model(self.XA0, self.temp_control)
         elif not self.const_temp and self.doe_solve:
-            self.model = reform_batch_reactor_model_doe(self.XA0, self.temp_control)
+            self.model = reform_var_temp_doe_model(self.temp_control)
 
         return self.model
 
@@ -70,6 +71,7 @@ class BatchReactorExperiment(Experiment):
         if self.doe_solve:
             # label the experimental decision variables
             m.experiment_inputs = pyo.Suffix(direction=pyo.Suffix.LOCAL)
+            m.experiment_inputs[m.XA[0]] = None
             m.experiment_inputs.update(
                 (m.T_reparam[t], None) for t in m.t
             )
@@ -101,10 +103,10 @@ class BatchReactorExperiment(Experiment):
                 (m.XA[t], 0.01) for t in m.t
             )
             m.measurement_error.update(
-                (m.XB[t], 1e-02) for t in m.t
+                (m.XB[t], 0.001) for t in m.t
             )
             m.measurement_error.update(
-                (m.XC[t], 1e-02) for t in m.t
+                (m.XC[t], 0.001) for t in m.t
             )
             m.measurement_error.update(
                 (m.XP[t], 0.01) for t in m.t
@@ -145,10 +147,10 @@ class BatchReactorExperiment(Experiment):
                 (m.XA[t], 0.01) for t in meas_time_points
             )
             m.measurement_error.update(
-                (m.XB[t], 1e-06) for t in meas_time_points
+                (m.XB[t], 0.001) for t in meas_time_points
             )
             m.measurement_error.update(
-                (m.XC[t], 1e-06) for t in meas_time_points
+                (m.XC[t], 0.001) for t in meas_time_points
             )
             m.measurement_error.update(
                 (m.XP[t], 0.01) for t in meas_time_points
@@ -169,14 +171,12 @@ class BatchReactorExperiment(Experiment):
         return m
 
 
-def reform_batch_reactor_model_doe(XA0, temp_control_points):
+def reform_var_temp_doe_model(temp_control_points):
     """
     Reformulates the variable temperature batch reactor model for parameter estimation
 
     Parameters
     ----------
-    XA0: float,
-        Initial mass fraction of species A
     temp_control_points: dict,
         Piecewise-linear profile of the reaction temperature (in R)
 
@@ -192,39 +192,38 @@ def reform_batch_reactor_model_doe(XA0, temp_control_points):
     model.t = dae.ContinuousSet(bounds=[0, 3])  # hour
 
     # define the model parameters
-    # define the model parameters
     model.alpha_1 = pyo.Var(bounds=(0, None), )
-    model.alpha_1.fix(22.600)
+    model.alpha_1.fix(23.80)
 
     model.alpha_2 = pyo.Var(bounds=(0, None), )
-    model.alpha_2.fix(28.296)
+    model.alpha_2.fix(24.46)
 
     model.alpha_3 = pyo.Var(bounds=(0, None), )
-    model.alpha_3.fix(37.020)
+    model.alpha_3.fix(35.61)
 
     model.E1 = pyo.Var(bounds=(0, None), )
-    model.E1.fix(120.526)
+    model.E1.fix(127.80)
 
     model.E2 = pyo.Var(bounds=(0, None), )
-    model.E2.fix(148.294)
+    model.E2.fix(125.98)
 
     model.E3 = pyo.Var(bounds=(0, None), )
-    model.E3.fix(201.293)
+    model.E3.fix(193.64)
 
     # add the mass fraction variables
-    model.XA = pyo.Var(model.t, bounds=(0, 1), initialize=XA0)
-    model.XB = pyo.Var(model.t, bounds=(0, 1), initialize=1 - XA0 - 4e-6)
-    model.XC = pyo.Var(model.t, bounds=(0, 1), initialize=1e-6)
-    model.XE = pyo.Var(model.t, bounds=(0, 1), initialize=1e-6)
-    model.XP = pyo.Var(model.t, bounds=(0, 1), initialize=1e-6)
-    model.XG = pyo.Var(model.t, bounds=(0, 1), initialize=1e-6)
+    model.XA = pyo.Var(model.t, bounds=(0, 1), initialize=0.3)
+    model.XB = pyo.Var(model.t, bounds=(0, 1), initialize=0.7)
+    model.XC = pyo.Var(model.t, bounds=(0, 1), initialize=0)
+    model.XE = pyo.Var(model.t, bounds=(0, 1), initialize=0)
+    model.XP = pyo.Var(model.t, bounds=(0, 1), initialize=0)
+    model.XG = pyo.Var(model.t, bounds=(0, 1), initialize=0)
 
     # add the temperature variables
-    model.T_reparam = pyo.Var(model.t, bounds=[1/6.8, 1/5.8], initialize=1/6)
+    model.T_reparam = pyo.Var(model.t, bounds=[1/6.8, 1/5.8],)
 
     # add the rate constants
-    model.k_reparam = pyo.Var(reaction_number, model.t, bounds=(0, None), initialize=1)
-    model.k = pyo.Var(reaction_number, model.t, bounds=(0, None), initialize=10)
+    model.k_reparam = pyo.Var(reaction_number, model.t, bounds=(0, None),)
+    model.k = pyo.Var(reaction_number, model.t, bounds=(0, None),)
 
     # calculate the reparameterized rate constants
     def k_reparam_rule(m, i, t):
@@ -247,138 +246,284 @@ def reform_batch_reactor_model_doe(XA0, temp_control_points):
         reaction_number, model.t, rule=k_rule
     )
 
-    # reaction rates
-    model.r = pyo.Var(reaction_number, model.t, bounds=(0, None), initialize=1)
-
-    # define the rate of reactions
-    def reaction_rate_rule(m, i, t):
-        if i == 1:
-            return m.r[i, t] == m.k[i, t] * m.XA[t] * m.XB[t]
-        elif i == 2:
-            return m.r[i, t] == m.k[i, t] * m.XB[t] * m.XC[t]
-        else:
-            return m.r[i, t] == m.k[i, t] * m.XC[t] * m.XP[t]
-
-    model.reaction_rate = pyo.Constraint(
-        reaction_number, model.t, rule=reaction_rate_rule
-    )
-
     # add the differential equations for XA, XB, XC, XE, XP, and XG
     model.dXA = dae.DerivativeVar(model.XA, wrt=model.t)
     model.dXB = dae.DerivativeVar(model.XB, wrt=model.t)
     model.dXC = dae.DerivativeVar(model.XC, wrt=model.t)
     model.dXE = dae.DerivativeVar(model.XE, wrt=model.t)
-    # model.dXP = dae.DerivativeVar(model.XP, wrt=model.t)
+    model.dXP = dae.DerivativeVar(model.XP, wrt=model.t)
     model.dXG = dae.DerivativeVar(model.XG, wrt=model.t)
 
     @model.Constraint(model.t)
     def xa_rate_ode(m, t):
+        if t == m.t.first():
+            return pyo.Constraint.Skip
         return m.dXA[t] == - m.k[1, t] * m.XA[t] * m.XB[t]
 
     @model.Constraint(model.t)
     def xb_rate_ode(m, t):
+        if t == m.t.first():
+            return pyo.Constraint.Skip
         return m.dXB[t] == - (m.k[1, t] * m.XA[t] * m.XB[t] + m.k[2, t] * m.XB[t] * m.XC[t])
 
     @model.Constraint(model.t)
     def xc_rate_ode(m, t):
+        if t == m.t.first():
+            return pyo.Constraint.Skip
         return m.dXC[t] == 2 * m.k[1, t] * m.XA[t] * m.XB[t] - 2 * m.k[2, t] * m.XB[t] * m.XC[t] - m.k[3, t] * m.XC[t] * m.XP[t]
 
     @model.Constraint(model.t)
     def xe_rate_ode(m, t):
+        if t == m.t.first():
+            return pyo.Constraint.Skip
         return m.dXE[t] == 2 * m.k[2, t] * m.XB[t] * m.XC[t]
 
     @model.Constraint(model.t)
+    def xp_rate_ode(m, t):
+        if t == m.t.first():
+            return pyo.Constraint.Skip
+        return m.dXP[t] == m.k[2, t] * m.XB[t] * m.XC[t] - 0.5 * m.k[3, t] * m.XC[t] * m.XP[t]
+
+    @model.Constraint(model.t)
     def xg_rate_ode(m, t):
+        if t == m.t.first():
+            return pyo.Constraint.Skip
         return m.dXG[t] == 1.5 * m.k[3, t] * m.XC[t] * m.XP[t]
 
-    # add the mass fraction constraint
-    @model.Constraint(model.t)
-    def sum_mass_fraction(m, t):
-        return m.XA[t] + m.XB[t] + m.XC[t] + m.XE[t] + m.XG[t] + m.XP[t] == 1
-
+    # # add the mass fraction constraint
     # @model.Constraint(model.t)
-    # def xp_rate_ode(m, t):
-    #     return m.dXP[t] == m.k[2, t] * m.XB[t] * m.XC[t] - 0.5 * m.k[3, t] * m.XC[t] * m.XP[t]
+    # def sum_mass_fraction(m, t):
+    #     return m.XA[t] + m.XB[t] + m.XC[t] + m.XE[t] + m.XG[t] + m.XP[t] == 1
 
     # # fix the initial conditions
     t0 = model.t.first()
-    model.XA[t0].fix(XA0)
-    model.XB[t0].fix(1 - XA0)
+    model.XB_init = pyo.Constraint(expr=model.XB[t0] == 1 - model.XA[t0])
     model.XC[t0].fix(0.0)
     model.XE[t0].fix(0.0)
-    # model.XP[t0].fix(0.0)
+    model.XP[t0].fix(0.0)
     model.XG[t0].fix(0.0)
 
     # discretize the model
     disc = pyo.TransformationFactory("dae.finite_difference")
     disc.apply_to(model, nfe=60, scheme="BACKWARD")
 
-    # add a piecewise-linear temperature profile
+    # initialize the temperature using the profile of the prior experiment
     for t in model.t:
         if t < 0.5:
             temp_at_time_t = temp_control_points["0.0"] + 0.2 * t
 
-        elif t < 1.0:
-            temp_at_time_t = temp_control_points["0.5"] + 0.6 * (t - 0.5)
+        elif t < 1.5:
+            temp_at_time_t = temp_control_points["0.5"] + 0.4 * (t - 0.5)
 
         else:
-            temp_at_time_t = 6.2
+            temp_at_time_t = temp_control_points["1.5"]
 
-        model.T_reparam[t].fix(1 / temp_at_time_t)
+        model.T_reparam[t].set_value(1 / temp_at_time_t)
 
     return model
 
-# define the prior information (Fisher information matrix)
-prior_FIM = np.array([
-    [1.54923906e+10, 1.46769057e+09, -1.70116610e+09, -2.64078888e+09, -2.47886401e+08, 2.88800421e+08],
-    [1.46769032e+09, 2.62585135e+09, 2.82304932e+08, -2.50870575e+08, -4.46282077e+08, -4.80859752e+07],
-    [-1.70116614e+09, 2.82304901e+08, 3.17900344e+08, 2.88573724e+08, -4.84468430e+07, -5.37121274e+07],
-    [-2.64078888e+09, -2.50870616e+08, 2.88573717e+08, 4.50173794e+08, 4.23788425e+07, -4.89966831e+07],
-    [-2.47886360e+08, -4.46282077e+08, -4.84468482e+07, 4.23788354e+07, 7.58506979e+07, 8.25060451e+06],
-    [2.88800428e+08, -4.80859700e+07, -5.37121274e+07, -4.89966843e+07, 8.25060362e+06, 9.07666881e+06]
-])
 
-temp_control = {"0.0":5.8, "0.5":5.9, "1.0":6.2, "1.5":6.4, "2.0":6.6, "3.0":6.8}
+def main(doe_objective):
+    """
+    Solves the experimental design problem for XA(0) and T(t) for 0 <= t <= 3
 
-# create an experiment object
-experiment = BatchReactorExperiment(XA0=0.4, temp_control=temp_control,
-                                    const_temp=False, doe_solve=True)
+    Parameters
+    ----------
+    doe_objective: str,
+        Objective function for the experimental design formulation
+    :param objective_option:
+    :return:
+    """
 
-# Use a central difference, with step size 1e-3
-fd_formula = "central"
-step_size = 1e-3
+    prior_FIM = np.array([
+        [1.642115e+04, 1.318845e+03, -1.527089e+03, -2.798949e+03, -2.228744e+02, 2.595965e+02],
+        [1.318845e+03, 3.470710e+03, -2.317680e+02, -2.266803e+02, -5.883556e+02, 3.791954e+01],
+        [-1.527089e+03, -2.317680e+02, 7.473955e+02, 2.597131e+02, 3.765902e+01, -1.255188e+02],
+        [-2.798949e+03, -2.266803e+02, 2.597131e+02, 4.771091e+02, 3.831287e+01, -4.415367e+01],
+        [-2.228744e+02, -5.883556e+02, 3.765902e+01, 3.831287e+01, 9.974374e+01, -6.155345e+00],
+        [2.595965e+02, 3.791954e+01, -1.255188e+02, -4.415367e+01, -6.155345e+00, 2.108267e+01]
+    ])
 
-# Use the determinant objective
-objective_option = "determinant"
+    temp_control = {
+        "0.0": 5.8,
+        "0.5": 5.9,
+        "1.5": 6.3,
+    }
 
-# Create the DesignOfExperiments object
-doe_obj = DesignOfExperiments(
-    experiment,
-    fd_formula=fd_formula,
-    step=step_size,
-    objective_option=objective_option,
-    prior_FIM=prior_FIM,
-    tee=True,)
+    experiment = BatchReactorExperiment(
+        temp_control=temp_control,
+        const_temp=False,
+        doe_solve=True,
+    )
 
-doe_obj.run_doe()
+    doe_obj = DesignOfExperiments(
+        experiment=experiment,
+        fd_formula="backward",
+        step=1e-2,
+        use_grey_box_objective=True,
+        objective_option=doe_objective,
+        prior_FIM=prior_FIM,
+        tee=True,
+        grey_box_tee=True,
+    )
 
-solved_model = doe_obj.model.scenario_blocks[0]
+    doe_obj.run_doe()
 
-# Extract time and temperature
-t_vals = list(solved_model.t)
-T_vals = [pyo.value(solved_model.T_reparam[t]) for t in t_vals]
+    solved_model = doe_obj.model.scenario_blocks[0]
+    return solved_model
 
-# If you want actual temperature instead of reparameterized (1/T):
-T_actual = [1 / val for val in T_vals]
 
-# Plot
-plt.figure()
-plt.plot(t_vals, T_actual,)
-plt.xlabel('Time (hr)')
-plt.ylabel('Temperature (R)')
-plt.title('Optimal Temperature Profile')
-plt.grid()
-plt.show()
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
+
+    # define the experimental design objectives
+    doe_objectives = ["trace", "determinant", "minimum_eigenvalue", "condition_number"]
+    # doe_objectives = ["trace", "determinant"]
+
+    for objective in doe_objectives:
+        solved_model = main(objective)
+        print("The optimal XA0 is:", pyo.value(solved_model.XA[0]))
+
+        # Extract the time and temperature
+        t_vals = list(solved_model.t)
+        reparm_temp_vals = [pyo.value(solved_model.T_reparam[t]) for t in t_vals]
+        XA_vals = [pyo.value(solved_model.XA[t]) for t in t_vals]
+        XB_vals = [pyo.value(solved_model.XB[t]) for t in t_vals]
+        XC_vals = [pyo.value(solved_model.XC[t]) for t in t_vals]
+        XP_vals = [pyo.value(solved_model.XP[t]) for t in t_vals]
+        XE_vals = [pyo.value(solved_model.XE[t]) for t in t_vals]
+        XG_vals = [pyo.value(solved_model.XG[t]) for t in t_vals]
+
+        # create a dataframe to store the results
+        results_df = pd.DataFrame({
+            "Time (hr)": t_vals,
+            "Temperature (1/R)": reparm_temp_vals,
+            "XA": XA_vals,
+            "XB": XB_vals,
+        })
+
+        # save to a csv file
+        results_df.to_csv(f".\{objective}_optimal_design_cond.csv", index=False)
+
+        # get the temperature
+        temp_actual = [1 / val for val in reparm_temp_vals]
+
+        # Plot
+        fig, ax1 = plt.subplots(figsize=(6.5, 5))
+
+        ax1.plot(t_vals, XA_vals, label='X$_\mathbf{A}$', color='blue', linewidth=2)
+        ax1.plot(t_vals, XB_vals, label='X$_\mathbf{B}$', color='orange', linewidth=2)
+        ax1.plot(t_vals, XC_vals, label='X$_\mathbf{C}$', color='tab:cyan', linewidth=2)
+        ax1.plot(t_vals, XP_vals, label='X$_\mathbf{P}$', color='green', linewidth=2)
+        ax1.plot(t_vals, XE_vals, label='X$_\mathbf{E}$', color='purple', linewidth=2)
+        ax1.plot(t_vals, XG_vals, label='X$_\mathbf{G}$', color='black', linewidth=2)
+
+        ax1.set_xlabel('Time (hr)', fontsize=16, fontweight='bold')
+        ax1.set_ylabel('Mass Fraction', fontsize=16, fontweight='bold')
+        ax1.tick_params(axis='x', labelsize=16, direction="in", top=True, right=True)
+        ax1.tick_params(axis='y', labelsize=16, direction="in", top=True, right=True)
+
+        # set the secondary axis: mass fractions
+        ax2 = ax1.twinx()
+        # set the primary axis: temperature
+        ax2.plot(t_vals, temp_actual, color='red', linewidth=3)
+        ax2.set_ylabel('Temperature (R)', color='red', fontsize=16, fontweight='bold')
+        ax2.tick_params(axis='y', labelsize=16, labelcolor='red', direction="in", top=True, right=True)
+
+        # add legend
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(f".\design_profile_{objective}.png")
+        plt.show()
+
+# if __name__ == "__main__":
+#     # define the prior information (Fisher information matrix)
+#     prior_FIM = np.array([
+#         [1.64106296e+04, 1.30303771e+03, -1.50599157e+03, -2.79713263e+03, -2.20184400e+02, 2.55975519e+02],
+#         [1.30303771e+03, 3.46262170e+03, -2.25070337e+02, -2.24084396e+02, -5.86688258e+02, 3.65194461e+01],
+#         [-1.50599157e+03, -2.25070337e+02, 7.37528512e+02, 2.56253290e+02, 3.62941623e+01, -1.23642432e+02],
+#         [-2.79713263e+03, -2.24084396e+02, 2.56253290e+02, 4.76796803e+02, 3.78710322e+01, -4.35593824e+01],
+#         [-2.20184400e+02, -5.86688258e+02, 3.62941623e+01, 3.78710322e+01, 9.94125242e+01, -5.88060853e+00],
+#         [2.55975519e+02, 3.65194461e+01, -1.23642432e+02, -4.35593824e+01, -5.88060853e+00, 2.07310442e+01]
+#     ])
+#
+#     temp_control = {"0.0":5.8, "0.5":5.9, "1.0":6.2, "1.5":6.4, "2.0":6.6, "3.0":6.8}
+#
+#     # create an experiment object
+#     experiment = BatchReactorExperiment(XA0=0.4, temp_control=temp_control,
+#                                         const_temp=False, doe_solve=True)
+#
+#     # Use a central difference, with step size 1e-3
+#     fd_formula = "central"
+#     step_size = 1e-2
+#
+#     # Use the determinant objective
+#     objective_option = "pseudo_trace"
+#
+#     # Create the DesignOfExperiments object
+#     # doe_obj = DesignOfExperiments(
+#     #     experiment,
+#     #     fd_formula=fd_formula,
+#     #     step=step_size,
+#     #     objective_option=objective_option,
+#     #     prior_FIM=prior_FIM,
+#     #     tee=True,)
+#
+#     doe_obj = DesignOfExperiments(
+#         experiment=experiment,
+#         fd_formula=fd_formula,
+#         step=step_size,
+#         use_grey_box_objective=True,  # Comment out if normal
+#         scale_constant_value=1,
+#         scale_nominal_param_value=True,
+#         objective_option=objective_option,
+#         prior_FIM=prior_FIM,
+#         tee=True,
+#         grey_box_tee=True,
+#         )
+#
+#     doe_obj.run_doe()
+#
+#     solved_model = doe_obj.model.scenario_blocks[0]
+#
+#     # Extract time and temperature
+#     t_vals = list(solved_model.t)
+#     T_vals = [pyo.value(solved_model.T_reparam[t]) for t in t_vals]
+#     XA_vals = [pyo.value(solved_model.XA[t]) for t in t_vals]
+#     XB_vals = [pyo.value(solved_model.XB[t]) for t in t_vals]
+#     XP_vals = [pyo.value(solved_model.XP[t]) for t in t_vals]
+#     XE_vals = [pyo.value(solved_model.XE[t]) for t in t_vals]
+#     XG_vals = [pyo.value(solved_model.XG[t]) for t in t_vals]
+#
+#     # If you want actual temperature instead of reparameterized (1/T):
+#     T_actual = [1 / val for val in T_vals]
+#
+#     # Plot
+#     fig, ax1 = plt.subplots()
+#
+#     # set the primary axis: temperature
+#     ax1.plot(t_vals, T_actual, color='red', linewidth=3)
+#     ax1.set_xlabel('Time (hr)', fontsize=14, fontweight='bold')
+#     ax1.set_ylabel('Temperature (R)', color='red', fontsize=14, fontweight='bold')
+#     ax1.tick_params(axis='x', labelsize=14, direction="in", top=True, right=True)
+#     ax1.tick_params(axis='y', labelsize=14, labelcolor='red', direction="in", top=True, right=True)
+#
+#     # set the secondary axis: mass fractions
+#     ax2 = ax1.twinx()
+#
+#     ax2.plot(t_vals, XA_vals, label='XA', color='blue')
+#     ax2.plot(t_vals, XB_vals, label='XB', color='orange')
+#     ax2.plot(t_vals, XP_vals, label='XP', color='cyan')
+#     ax2.plot(t_vals, XE_vals, label='XE', color='purple')
+#     ax2.plot(t_vals, XG_vals, label='XG', color='brown')
+#
+#     ax2.set_ylabel('Mass Fraction', fontsize=14, fontweight='bold')
+#     ax2.tick_params(axis='y', labelsize=14, direction="in", top=True, right=True)
+#
+#     # add legend
+#     plt.grid()
+#     plt.show()
 
 
 # # Sensitivity analysis
